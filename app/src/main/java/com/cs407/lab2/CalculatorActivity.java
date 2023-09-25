e9bdf7e715189298ae617f9f41361048fe06ce7d
Alex-He-3 <82226700+Alex-He-3@users.noreply.github.com>
2023-09-25 09:12:47 -0500

    initial commit

--- app/src/main/java/com/cs407/lab2/CalculatorActivity.java
+++ app/src/main/java/com/cs407/lab2/CalculatorActivity.java
@@ -2,20 +2,23 @@ package com.cs407.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
 
-    TextView textView;
+    TextView textview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculator_activity);
-        textView=(TextView) findViewById(R.id.textView);
-        Intent intent=getIntent();
-        String str=intent.getStringExtra("message");
-        textView.setText("Hello "+str);
+
+        textview = (TextView) findViewById(R.id.textView);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        textview.setText(message);
     }
 }
--- app/src/main/java/com/cs407/lab2/MainActivity.java
+++ app/src/main/java/com/cs407/lab2/MainActivity.java
@@ -2,7 +2,6 @@ package com.cs407.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,16 +17,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void clickFunction(View view){
-        EditText myTextField = (EditText) findViewById(R.id.editTextText);
-    //    Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_SHORT).show();
-        goToActivity(myTextField.getText().toString());
-
-    }
-
-    public void goToActivity(String s){
-        Intent intent =new Intent(this, CalculatorActivity.class);
+    public void goToActivity(String s) {
+        Intent intent = new Intent(this, CalculatorActivity.class);
         intent.putExtra("message", s);
         startActivity(intent);
+    }
+
+    public void clickAdd(View view) {
+        float first = Float.parseFloat(((EditText) findViewById(R.id.numberOne)).getText().toString());
+        float second =Float.parseFloat(((EditText) findViewById(R.id.numberTwo)).getText().toString());
+
+        goToActivity(""+ (first + second));
+    }
+
+    public void clickMinus(View view) {
+        float first = Float.parseFloat(((EditText) findViewById(R.id.numberOne)).getText().toString());
+        float second = Float.parseFloat(((EditText) findViewById(R.id.numberTwo)).getText().toString());
+
+        goToActivity(""+ (first - second));
+    }
+
+    public void clickMulti(View view) {
+        float first = Float.parseFloat(((EditText) findViewById(R.id.numberOne)).getText().toString());
+        float second = Float.parseFloat(((EditText) findViewById(R.id.numberTwo)).getText().toString());
+
+        goToActivity(""+ (first * second));
+    }
+
+    public void clickDivided(View view) {
+        float first = Float.parseFloat(((EditText) findViewById(R.id.numberOne)).getText().toString());
+        float second = Float.parseFloat(((EditText) findViewById(R.id.numberTwo)).getText().toString());
+
+        if (second == 0) {
+            Toast.makeText(this, "Cannot divide by zero", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            goToActivity(""+ (first / second));
+        }
     }
 }
